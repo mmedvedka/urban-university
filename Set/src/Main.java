a19abdc13074133716fe041ae3c5e1e2ef2c8ac6
mmedvedka <60501805+mmedvedka@users.noreply.github.com>
2024-12-17 16:22:50 +0300

    Set. HashSet. LinkedHashSet.

---
+++ Set/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+    public static HashSet<String> GetCommonElements (HashSet<String> set1, HashSet<String> set2){
+        set1.retainAll(set2);
+        return set1;
+    }
+
+    public static void main(String[] args) {
+        /*
+            1.Создайте два набора, хранящих в себе элементы строкового типа, характеризующие цвет чего – либо.
+              Напишите метод для сравнения двух наборов и сохранения элементов, которые одинаковы для обоих наборов.
+            2.Создайте множество целых чисел. Необходимо вывести в консоль все его элементы, которые больше 15 и являются четными.
+              Остальные элементы набора тоже необходимо вывести в консоль, но каждый из них разделив на 2.
+         */
+        {
+            //  1.
+            System.out.println("1.");
+            HashSet<String> set1 = new HashSet<String>();
+            HashSet<String> set2 = new HashSet<String>();
+
+            String[] colorNames1 = {"red", "blue", "green", "orange", "black"};
+            String[] colorNames2 = {"red", "blue", "green", "pink","white"};
+
+            for(String color: colorNames1){
+                set1.add(color);
+            }
+
+            for(String color: colorNames2){
+                set2.add(color);
+            }
+
+            System.out.println("Common elements of set1 and set2: "+GetCommonElements (set1, set2));
+        }
+        {
+            //  2.
+            System.out.println("2.");
+
+            Integer[] numbers = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+            HashSet<Integer> set = new HashSet<>();
+
+            for(Integer num: numbers){
+                set.add(num);
+            }
+
+            System.out.println("Common elements of set1 and set2: "+GetCommonElements (set1, set2));
+        }
+
+    }
+}
